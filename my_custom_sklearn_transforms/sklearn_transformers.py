a3e49d427478cb426698b9d68ff299628ca96cf8
elnw <xxelnwxx@gmail.com>
2020-08-23 01:17:40 -0500

    Agregando transformacion por Hot Encoding

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,19 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+    
+class ConvertCatColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+        self.jobs_encoder = LabelBinarizer()
+
+    def fit(self, X, y=None):
+        self.jobs_encoder.fit(X[self.columns])
+        return self
+    
+    def transform(self, X):
+        transformed = self.jobs_encoder.transform(X[self.columns])
+        ohe_df = pd.DataFrame(transformed)
+        X.drop(columns=self.columns, inplace=True)
+        return pd.concat([X, ohe_df], axis=1)
